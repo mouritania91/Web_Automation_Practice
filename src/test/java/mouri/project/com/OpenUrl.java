27687e2811b25f8df85f5cc60ad23e0b29e78de4
mouritania91 <mtushi91@gmail.com>
2024-06-03 23:58:13 +0200

    2nd day update

--- src/test/java/mouri/project/com/OpenUrl.java
+++ src/test/java/mouri/project/com/OpenUrl.java
@@ -7,7 +7,7 @@ public class OpenUrl extends BaseDriver{
 	
 	@Test
 	public void open() throws InterruptedException {
-		driver.get("https://www.selenium.dev/downloads/");	
+		driver.get("https://www.bdjobs.com/");	
 		Thread.sleep(5000);
 	}
 	
